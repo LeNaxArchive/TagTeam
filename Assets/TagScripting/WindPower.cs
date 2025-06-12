ccf964c4eeb1b3e5ad9b853130f8ebb109eb8bc2
LeNaxArchive <wazini.nax.@gmail.com>
2025-06-12 18:06:34 +0800

    Animation

--- Assets/TagScripting/WindPower.cs
+++ Assets/TagScripting/WindPower.cs
@@ -2,12 +2,20 @@ using UnityEngine;
 
 public class WindPower : MonoBehaviour
 {
-    public float zDirection = -100;
+    public float pushForce = 200;
+    //public float zDirection = -100;
     void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "Enemy" && other.attachedRigidbody)
         {
-            other.attachedRigidbody.AddForce(Vector3.forward * zDirection);
+            //other.attachedRigidbody.AddForce(Vector3.forward * zDirection);
+
+            // Calculate the direction from the center of the WindPower object to the enemy
+            Vector3 pushDirection = other.transform.position - transform.position;
+            // Normalize the direction to ensure consistent force regardless of distance
+            pushDirection.Normalize();
+            // Apply the force to the enemy's rigidbody
+            other.attachedRigidbody.AddForce(pushDirection * pushForce);
         }
     }
 }
